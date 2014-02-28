abccd07d24a5ff303e4e7790d02f0eef1b6d5913
Captain Sloth <yhchun1992@hotmail.com>
2014-02-28 20:39:07 +1300

    added spaceship, asteroid, spaceshipcontrols, asteroid spawn script

---
+++ 2DSpaceGame/Assets/Scripts/Asteroid/AsteroidSpawn.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidSpawn : MonoBehaviour {
+	/* Spawns asteroids at a spawn point
+	 * It will spawn asteroids at random y coordinates relative to the spawn point, but will stay within the y coordinate 
+	 * the camera.
+	 */
+
+	public float spawnInterval; //The time between spawning asteroids
+	private Camera camera; //Get the main camera
+	private float maxDistance; //The max distance to spawn away from camera on the Y coordinate
+	private float tick; //A tick occurs when tick >= spawnInterval. Spawn asteroid every time there's a tick.
+	// Use this for initialization
+	void Start () {
+		camera = Camera.main;
+		maxDistance = camera.transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tick += Time.deltaTime;
+		if(tick>= spawnInterval){
+			tick=0f;
+			spawnAsteroid(Random.Range(-maxDistance,maxDistance)+camera.transform.position.y);
+		}
+
+	}
+
+	/* Spawns an asteroid at location given a y coordinate*/
+	private void spawnAsteroid(float y){
+		GameObject newAsteroid = Instantiate(Resources.Load ("Prefabs/GameObjects/Asteroids/Asteroid"),new Vector3(transform.position.x,y,0),transform.rotation) as GameObject;
+		newAsteroid.rigidbody2D.AddForce(-Vector2.right* Random.Range (1,1000)+GameObject.FindGameObjectWithTag("Player").rigidbody2D.velocity);
+		newAsteroid.rigidbody2D.AddTorque(Random.Range(1,100));
+	
+	}
+}
---
+++ 2DSpaceGame/Assets/Scripts/Spaceship/SpaceshipControls.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceshipControls : MonoBehaviour {
+	/*This script  will be attached to the spaceship.
+	 * Its purpose is to control the movement of the spaceship.
+	 * The spaceship can rotate, accelerate and decelerate.
+	 * Rotating the space ship will make the ship rotate constantly until an opposite rotation
+	 * is used.
+	 */
+	public float acceleration; //The forward acceleration of the spaceship.
+	public float deceleration; //The backward acceleration of the spaceship.
+	public float rotationSpeed; //The rotation speed of the spaceship.
+	public float maxSpeed; //The Maximum acceleration speed. Current speed cannot exceed this amount.
+
+	private float currentSpeed; //The current speed of the spaceship.
+	private bool destroyed = false; //whether or not the spaceship has been destroyed.
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void FixedUpdate(){
+		if(!destroyed){
+			float angle = transform.eulerAngles.magnitude * Mathf.Deg2Rad;
+			float direction = Input.GetAxisRaw("Vertical");
+			float turn = Input.GetAxisRaw("Horizontal") * rotationSpeed;
+			Vector2 thrust;
+			if(direction > 0f){ //if moving forward
+				thrust = new Vector2((Mathf.Cos (angle) * acceleration), (Mathf.Sin (angle) * acceleration)) ;
+			}
+			else{ //moving backwards
+				thrust = new Vector2((Mathf.Cos (angle) * deceleration), (Mathf.Sin (angle) * deceleration)) ;
+			}
+			print (rigidbody2D.velocity.magnitude);
+			if(direction != 0f){
+				rigidbody2D.AddForce(thrust * direction * Time.deltaTime);
+			}
+			rigidbody2D.AddTorque(-turn * Time.deltaTime);
+			
+		}
+	}
+
+	private void accelerate(){
+	}
+
+	private void decelerate(){
+
+	}
+	private void rotate(){
+
+	}
+}
